cb9441f052d931229dc5494211c283446f405cb6
adrianromanski <adrian.romanski@codeweaver.pl>
2025-03-04 14:06:24 +0100

    tests: move list item and submenu tests

--- apps/a11y-components/src/app/components/menu /menu-list/menu-list.component.spec.ts
+++ apps/a11y-components/src/app/components/menu /menu-list/menu-list.component.spec.ts
@@ -1,21 +1,85 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MenuListComponent } from './menu-list.component';
+import { Component, viewChild } from '@angular/core';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { MenuListComponentHarness } from './menu-list.component.harness';
+import { MenuItem } from '../menu.component';
+
+@Component({
+  selector: 'app-test-menu-list-wrapper',
+  imports: [MenuListComponent],
+  template: ` <app-menu-list
+                [menuItems]="menuItems"
+                [isOpen]="true">
+              </app-menu-list>
+  `,
+})
+
+export class TestMenuListWrapperComponent {
+  menuListComponent = viewChild.required(MenuListComponent);
+
+  menuItems: MenuItem[] = [
+    { label: 'Home', isOpen: false },
+    { label: 'About', isOpen: false },
+    {
+      label: 'Services',
+      isOpen: false,
+      submenu: [
+        {
+          label: 'Web Design',
+          submenu: [{ label: 'Expensive' }, { label: 'Cheap' }],
+        },
+        { label: 'SEO' },
+      ],
+    },
+    { label: 'Contact', isOpen: false },
+  ];
+}
 
 describe('MenuListComponent', () => {
-  let component: MenuListComponent;
-  let fixture: ComponentFixture<MenuListComponent>;
+  let component: TestMenuListWrapperComponent;
+  let fixture: ComponentFixture<TestMenuListWrapperComponent>;
+  let harness: MenuListComponentHarness;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [MenuListComponent],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(MenuListComponent);
+    fixture = TestBed.createComponent(TestMenuListWrapperComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    harness =  await TestbedHarnessEnvironment
+      .loader(fixture)
+      .getHarness(MenuListComponentHarness);
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  /**
+   * A menu generally represents a grouping of common actions or functions that the user can invoke.
+   * The menu role is appropriate when a list of menu items is presented in a manner similar to a menu on a desktop application.
+   * Submenus, also known as pop-up menus, also have the role menu.
+   */
+  it('should have menu role for list of menu items', async () => {
+    const lists = await harness.getLists();
+    const attribute = await lists[0].getAttribute('role');
+    expect(attribute).toBe('menu');
+  });
+
+  it('should have menu role for sub menus', async () => {
+    await harness.clickSubmenu();
+    const lists = await harness.getLists();
+    const attribute = await lists[0].getAttribute('role');
+    expect(attribute).toBe('menu');
+  });
+
+  /**
+   * When a user activates a choice in a menu that has been opened, the menu usually closes.
+   * If the menu choice action invokes a submenu, the menu will remain open and the submenu is displayed.
+   */
+  it('should open submenu after clicking it', async () => {
+    await harness.clickSubmenu();
+    expect(component.menuListComponent().isOpen).toBe(true);
+    expect(component.menuListComponent().menuItems[2].isOpen).toBe(true);
   });
 });
+
--- apps/a11y-components/src/app/components/menu /menu.component.spec.ts
+++ apps/a11y-components/src/app/components/menu /menu.component.spec.ts
@@ -4,67 +4,6 @@ import { Component, viewChild } from '@angular/core';
 import { MenuComponent } from './menu.component';
 import {TestbedHarnessEnvironment} from '@angular/cdk/testing/testbed';
 
-describe('TestMenuWrapperComponent', () => {
-  let fixture: ComponentFixture<TestMenuWrapperComponent>;
-  let harness: MenuComponentHarness;
-  let loader;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [],
-      imports: [],
-    }).compileComponents();
-
-    fixture = TestBed.createComponent(TestMenuWrapperComponent);
-    loader = TestbedHarnessEnvironment.loader(fixture);
-    harness =  await loader.getHarness(MenuComponentHarness);
-    fixture.detectChanges();
-  });
-
-  /**
-   * A menu generally represents a grouping of common actions or functions that the user can invoke.
-   * The menu role is appropriate when a list of menu items is presented in a manner similar to a menu on a desktop application.
-   * Submenus, also known as pop-up menus, also have the role menu.
-   */
-  it('should have menu role for list of menu items', async () => {
-    await harness.toggleMenu();
-    const lists = await harness.getLists();
-    const attribute = await lists[0].getAttribute('role');
-    expect(attribute).toBe('menu');
-  });
-
-  it('should have menu role for sub menus', async () => {
-    await harness.toggleMenu();
-    await harness.clickSubmenu();
-    const lists = await harness.getLists();
-    const attribute = await lists[0].getAttribute('role');
-    expect(attribute).toBe('menu');
-  });
-
-  /**
-   * When a user activates a choice in a menu that has been opened, the menu usually closes.
-   * If the menu choice action invokes a submenu, the menu will remain open and the submenu is displayed.
-   */
-  it('should close the menu after choosing item', async () => {
-    await harness.toggleMenu();
-    await harness.clickItem(1);
-    expect(fixture.componentInstance.menuComponent().isOpen).toBe(false);
-  });
-
-  it('should not close the menu after clicking submenu', async () => {
-    await harness.toggleMenu();
-    await harness.clickSubmenu();
-    expect(fixture.componentInstance.menuComponent().isOpen).toBe(true);
-  });
-
-  it('should open submenu after clicking it', async () => {
-    await harness.toggleMenu();
-    await harness.clickSubmenu();
-    expect(fixture.componentInstance.menuComponent().isOpen).toBe(true);
-    expect(fixture.componentInstance.menuComponent().menuItems[2].isOpen).toBe(true);
-  });
-
-});
 @Component({
   selector: 'app-test-menu-wrapper',
   imports: [MenuComponent],
@@ -73,3 +12,50 @@ describe('TestMenuWrapperComponent', () => {
 export class TestMenuWrapperComponent {
   menuComponent = viewChild.required(MenuComponent);
 }
+
+describe('TestMenuWrapperComponent', () => {
+  let fixture: ComponentFixture<TestMenuWrapperComponent>;
+  let harness: MenuComponentHarness;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [],
+      imports: [],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(TestMenuWrapperComponent);
+    harness =  await TestbedHarnessEnvironment.loader(fixture).getHarness(MenuComponentHarness);
+    fixture.detectChanges();
+  });
+
+  it('should open the menu after clicking the button', async () => {
+    await harness.toggleMenu();
+    expect(fixture.componentInstance.menuComponent().isOpen).toBe(true);
+  });
+
+  it('should close the menu after clicking the button', async () => {
+    await harness.toggleMenu();
+    expect(fixture.componentInstance.menuComponent().isOpen).toBe(true);
+    await harness.toggleMenu();
+    expect(fixture.componentInstance.menuComponent().isOpen).toBe(false);
+  });
+
+  /**
+   * When a user activates a choice in a menu that has been opened, the menu usually closes.
+   * If the menu choice action invokes a submenu, the menu will remain open and the submenu is displayed.
+   */
+  it('should close the menu after clicking item', async () => {
+    await harness.toggleMenu();
+    expect(fixture.componentInstance.menuComponent().isOpen).toBe(true);
+    await harness.clickItem(1);
+    expect(fixture.componentInstance.menuComponent().isOpen).toBe(false);
+  });
+
+  it('should not close the menu if clicked on submenu', async () => {
+    await harness.toggleMenu();
+    expect(fixture.componentInstance.menuComponent().isOpen).toBe(true);
+    await harness.clickSubmenu();
+    expect(fixture.componentInstance.menuComponent().isOpen).toBe(true);
+  });
+});
+
