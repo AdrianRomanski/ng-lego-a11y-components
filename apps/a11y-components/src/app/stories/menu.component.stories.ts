1db8fc285f3283d746ab80e6e8f919876a67d23e
adrianromanski <adrian.romanski@codeweaver.pl>
2025-03-29 10:16:27 +0100

    feat: lego as publishable library

--- apps/a11y-components/src/app/stories/menu.component.stories.ts
+++ apps/a11y-components/src/app/stories/menu.component.stories.ts
@@ -1,6 +1,6 @@
 import { componentWrapperDecorator, Meta, StoryObj } from '@storybook/angular';
 import { within, userEvent, waitFor } from '@storybook/testing-library';
-import { MenuComponent, MenuItem } from './menu.component';
+import { MenuComponent, MenuItem } from '@adrian-romanski/ng-lego-a11y';
 
 const meta: Meta<MenuComponent> = {
   component: MenuComponent,
