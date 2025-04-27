fd7e1c1008c4a55722475aa028f282280c95741a
adrian-romanski <muszynianka1337@gmail.com>
2025-04-27 12:03:02 +0200

    feat: add storybook basic styling

--- apps/storybook/.storybook/preview.ts
+++ apps/storybook/.storybook/preview.ts
@@ -2,7 +2,31 @@ import type { Preview } from '@storybook/angular';
 import { componentWrapperDecorator } from '@storybook/angular';
 
 const preview: Preview = {
-  decorators: [componentWrapperDecorator((story) => `<div style="margin: 3em">${story}</div>`)],
+  decorators: [componentWrapperDecorator((story) => `
+    <div style="
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      min-height: 90vh;
+      background: linear-gradient(135deg, #f5f7fa 0%, #c3cfe2 100%);
+      padding: 2rem;
+    ">
+      <div style="
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        min-width: 20vw;
+        min-height: 20vh;
+        background: white;
+        padding: 2rem;
+        border-radius: 1rem;
+        box-shadow: 0 10px 20px rgba(0, 0, 0, 0.1);
+        max-width: 100%;
+      ">
+        ${story}
+      </div>
+    </div>
+  `)],
 };
 
 export default preview;
