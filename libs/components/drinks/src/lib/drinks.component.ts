1b31d9fcc82bf2a4a6d50636b00cd1aa738f7d21
adrian-romanski <muszynianka1337@gmail.com>
2025-04-28 19:21:25 +0200

    feat: live coding 3

--- libs/components/drinks/src/lib/drinks.component.ts
+++ libs/components/drinks/src/lib/drinks.component.ts
@@ -1,11 +1,30 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal, Signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+export interface Drink {
+  name: string;
+}
 
 @Component({
   selector: 'lego-components-drinks',
   imports: [CommonModule],
-  template: `<p>drinks works!</p>`,
+  template: `
+     <button>
+       Perfect Drinks
+     </button>
+     <ul>
+       @for (drink of drinks(); track drink.name) {
+         <li>{{drink.name}}</li>
+       }
+     </ul>
+  `,
   styleUrl: './drinks.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DrinksComponent {}
+export class DrinksComponent {
+  drinks: Signal<Drink[]> = signal([
+    {name: 'Cola'},
+    {name: 'Coffee'},
+    {name: 'Beer'}
+  ])
+}
