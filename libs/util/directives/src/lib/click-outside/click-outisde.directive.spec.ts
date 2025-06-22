b8dee4ae3239fd45ef81147c6c9bcee289f5bf2e
adrianromanski <adrian.romanski@codeweaver.pl>
2025-06-22 19:29:59 +0200

    fix(util): directive selector causing failing tests

--- libs/util/directives/src/lib/click-outside/click-outisde.directive.spec.ts
+++ libs/util/directives/src/lib/click-outside/click-outisde.directive.spec.ts
@@ -6,7 +6,7 @@ import { ClickOutsideDirective } from './click-outside.directive';
   imports: [ClickOutsideDirective],
   template: `
     <div
-       legoComponentsClickOutside
+       legoUtilClickOutside
        (clickOutside)="onOutsideClick()">
     </div>`,
 })
