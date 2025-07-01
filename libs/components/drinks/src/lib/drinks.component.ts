8d41e2974611fbe08ec0603b9f7936db7aa0ce55
adrian-romanski <muszynianka1337@gmail.com>
2025-07-01 19:39:17 +0200

    feat(components): live coding

--- libs/components/drinks/src/lib/drinks.component.ts
+++ libs/components/drinks/src/lib/drinks.component.ts
@@ -1,8 +1,9 @@
 import {
   ChangeDetectionStrategy,
-  Component, output,
+  Component, effect, ElementRef,
+  output,
   signal,
-  Signal,
+  Signal, viewChild,
   WritableSignal
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -16,30 +17,48 @@ export interface Drink {
   selector: 'lego-components-drinks',
   imports: [CommonModule, ClickOutsideDirective],
   template: `
-    <span
-      class="content-trigger"
+    <button
+      #trigger
+      aria-haspopup="menu"
+      aria-controls="drinks"
+      aria-label="Best Drinks in Wroclaw"
       legoComponentsClickOutside
       (clickOutside)="open.set(false)"
       (click)="open.set(!this.open())"
     >
-      Best Drinks
-    </span>
+      Cheap
+    </button>
     @if (open()) {
-    <div class="content-wrapper">
-      @for (drink of drinks(); track drink.name) {
-      <div class="content" (click)="onDrinkClick(drink)">
-        <span [ngClass]="drink.isHot ? 'hot' : 'cold'" class="content-text">{{
-          drink.name
-        }}</span>
-      </div>
-      }
-    </div>
+      <ul
+        #menu
+        aria-label="Great Drinks"
+        tabindex="-1"
+        role="menu"
+        id="drinks">
+        @for (drink of drinks(); track drink.name) {
+        <li
+          (keydown)="onListItemKeyDown($event, drink)"
+          tabindex="-1"
+          role="menuitem"
+          (click)="onDrinkClick(drink)">
+          <span aria-hidden="true">{{drink.isHot ? '☕' : '🥤'}}</span>
+          <span [attr.aria-label]="drink.isHot
+          ? 'Please take care this is very hot'
+          : 'Nicely chilled'" class="content-text">{{
+            drink.name
+          }}</span>
+        </li>
+        }
+      </ul>
     }
   `,
   styleUrl: './drinks.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DrinksComponent {
+  trigger: Signal<ElementRef> = viewChild.required('trigger');
+  menu: Signal<ElementRef | undefined> = viewChild('menu');
+
   drinks: Signal<Drink[]> = signal([
     { name: 'Coffee', isHot: true },
     { name: 'Cola', isHot: false },
@@ -50,8 +69,35 @@ export class DrinksComponent {
 
   selectDrink = output<Drink>();
 
+  constructor() {
+    effect(() => {
+      if(this.open() && this.menu()) {
+        this.menu()?.nativeElement.querySelectorAll('li')[0].focus();
+      }
+    });
+  }
+
   protected onDrinkClick(drink: Drink): void {
     this.selectDrink.emit(drink);
     this.open.set(false);
+  }
+
+  protected onListItemKeyDown(event: KeyboardEvent, drink: Drink): void {
+    event.stopPropagation();
+    event.preventDefault();
+
+    const listItems = this.menu()?.nativeElement.querySelectorAll('li');
+
+    let index = Array.from(listItems).indexOf(document.activeElement as HTMLLIElement);
+
+    if(event.key === 'Enter' || ' ') {
+      this.onDrinkClick(drink);
+      this.trigger()?.nativeElement.focus();
+    } else if (event.key === 'Escape') {
+      this.trigger()?.nativeElement.focus();
+    } else if (event.key === 'ArrowDown') {
+      index = (index + 1) % listItems.length
+      listItems[index].focus();
+    }
   }
 }
