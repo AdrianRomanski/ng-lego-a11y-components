47ff31a18cefaa0bb91d17b2312d4fefa7dd99d8
adrian-romanski <muszynianka1337@gmail.com>
2025-07-03 19:28:43 +0200

    feat(components): live coding

--- libs/components/drinks/src/lib/drinks.component.ts
+++ libs/components/drinks/src/lib/drinks.component.ts
@@ -1,9 +1,9 @@
 import {
   ChangeDetectionStrategy,
-  Component,
+  Component, effect, ElementRef,
   input,
-  output,
-  signal,
+  output, Signal,
+  signal, viewChild
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ClickOutsideDirective } from './util/click-outside.directive';
@@ -16,46 +16,93 @@ export interface Drink {
   selector: 'lego-components-drinks',
   imports: [CommonModule, ClickOutsideDirective],
   template: `
-    <span
+    <button
+      #trigger
+      id="drink-trigger"
+      aria-haspopup="menu"
+      aria-controls="drinks"
+      aria-label="Best offer for drinks in office"
       class="content-trigger"
       legoComponentsClickOutside
       (clickOutside)="open.set(false)"
       (click)="open.set(!this.open())"
     >
-      Best Drinks
-    </span>
+      Best Offers
+    </button>
     @if (open()) {
-      <div
-        class="content-wrapper"
+    <ul
+      aria-labelledby="drink-trigger"
+      #menu tabindex="0" role="menu" id="drinks" class="content-wrapper">
+      @for (drink of drinks(); track drink.name) {
+      <li
+        tabindex="0"
+        role="menuitem"
+        class="content"
+        (keydown)="onMenuItemKeyDown($event, drink)"
+        (click)="onDrinkClick(drink)"
       >
-        @for (drink of drinks(); track drink.name) {
-          <div
-            class="content"
-            (click)="onDrinkClick(drink)"
-          >
-            <span
-              class="content-text"
-              [ngClass]="drink.isHot ? 'hot' : 'cold'"
-            >
-              {{drink.name }}
-            </span>
-          </div>
-        }
-      </div>
+        <span aria-hidden="true">{{drink.isHot ? '☕' : '🥤'}}</span>
+        <span [attr.aria-label]="drink.isHot ? 'Please take care is very hot' : 'nicely and chilled'" class="content-text">
+          {{ drink.name }}
+        </span>
+      </li>
+      }
+    </ul>
     }
   `,
   styleUrl: './drinks.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DrinksComponent {
+  menu: Signal<ElementRef | undefined> = viewChild('menu');
+  trigger: Signal<ElementRef> = viewChild.required('trigger');
+
   drinks = input.required<Drink[]>();
 
   selectDrink = output<Drink>();
 
   open = signal(false);
 
+  constructor() {
+    effect(() => {
+      if (this.open() && this.menu()) {
+        this.listItems[0].focus();
+      }
+    });
+
+  }
+
   protected onDrinkClick(drink: Drink): void {
     this.selectDrink.emit(drink);
     this.open.set(false);
+  }
+
+  protected onMenuItemKeyDown(event: KeyboardEvent, drink: Drink) {
+    if(event.key === 'Tab') {
+      this.open.set(false);
+    } else {
+      event.stopPropagation();
+      event.preventDefault();
+
+      let index = Array.from(this.listItems).indexOf(document.activeElement as HTMLLIElement);
+
+      if(event.key === 'Enter' || event.key === ' ') {
+        this.onDrinkClick(drink);
+        this.trigger().nativeElement.focus();
+      } else if (event.key === 'Escape') {
+        this.open.set(false);
+        this.trigger().nativeElement.focus();
+      } else if (event.key === 'ArrowDown') {
+        index = (index + 1) % this.listItems.length;
+        this.listItems[index].focus();
+      } else if (event.key === 'ArrowUp') {
+        index = (index - 1 + this.listItems.length) % this.listItems.length;
+        this.listItems[index].focus();
+      }
+    }
+   }
+
+  private get listItems() {
+    return this.menu()?.nativeElement.querySelectorAll('li');
   }
 }
