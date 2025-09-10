a62d79c31b295cb7eff1ce6510dd963fbd2f8760
adrian-romanski <muszynianka1337@gmail.com>
2025-09-10 18:19:14 +0200

    feat(libs): storybook as a library

---
+++ libs/storybook-lib/README.md
@@ -0,0 +1,7 @@
+# storybook-lib
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test storybook-lib` to execute the unit tests.
---
+++ libs/storybook-lib/src/lib/storybook-lib/storybook-lib.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-storybook-lib',
+  imports: [CommonModule],
+  templateUrl: './storybook-lib.component.html',
+  styleUrl: './storybook-lib.component.css',
+})
+export class StorybookLibComponent {}
